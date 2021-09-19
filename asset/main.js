40eae5a61a508e571bd0aa62f1c2ee5ac4677a4d
opikoenkz <opikoenkz@gmail.com>
2021-09-19 09:09:32 +0700

    Commit update pertama

--- asset/main.js
+++ asset/main.js
@@ -1,9 +1,33 @@
+/*----- Navbar Hamburger -----*/
+const menuBtn = document.querySelector(".menu-btn");
+const menuItems = document.querySelector(".navbar-menu");
+const menuItem = document.querySelectorAll(".nav-link");
+
+// main toggle
+menuBtn.addEventListener("click", () => {
+  toggle();
+});
+
+// toggle on item click if open
+menuItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (menuBtn.classList.contains("open")) {
+      toggle();
+    }
+  });
+});
+
+function toggle() {
+  menuBtn.classList.toggle("open");
+  menuItems.classList.toggle("open");
+}
+
 /*----- Modal Gallery -----*/
 function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-    var captionText = document.getElementById("caption");
-    captionText.innerHTML = element.alt; 
+  document.getElementById("img-pop-up").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  let captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
 }
 
 /*----- Accordion Album -----*/
@@ -11,15 +35,14 @@ var acc = document.getElementsByClassName("accordion-btn");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener ("click", function() {
-        this.classList.toggle("active");
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
 
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
-
